1903dcce1705bfbc0b5cdc4bbf76e7d516d7c3bd
grkumar82 <72323477+grkumar82@users.noreply.github.com>
2023-03-21 21:00:47 -0700

    Update ReadingAPI_stock_data.py

--- petProject/ReadingAPI_stock_data.py
+++ petProject/ReadingAPI_stock_data.py
@@ -1,74 +1,70 @@
-'''
+"""
 Queries API and saves results to a json file
-'''
+"""
 # github list of public APIs: https://github.com/public-apis/public-apis#finance
 # documentation: https://www.stockdata.org/documentation
 
 import requests
 import json
+import sys
 
 END_POINT = "https://api.stockdata.org/v1/data/eod"
+API_TOKEN = "xxxxx"
+PATH_NAME = "tsla_data.json"
 
 params = {
-    'api_token': "xxx",
-    'symbols': "TSLA",
-    'sort': "desc",
-    'date_from': "2018-01-01",
-    'date_to': "2022-12-31",
+    "api_token": API_TOKEN,
+    "symbols": "TSLA",
+    "sort": "desc",
+    "date_from": "2018-01-01",
+    "date_to": "2022-12-31",
 }
 
 response = requests.get(END_POINT, params=params)
 
-if response.status_code == 200:
-    data = response.json()
-    print(json.dumps(data, indent=4))  # Print formatted json
-else:
-    print('Error:', response.status_code)
+if response.status_code != 200:
+    print(f"Error {response.status_code} while retrieving data from {END_POINT}")
+    sys.exit(1)
 
-with open('tsla_data.json', 'w') as f:
+data = response.json()
+with open(PATH_NAME, "w") as f:
     json.dump(data, f)
+print(json.dumps(data, indent=4))
 
-    
-'''
+
+"""
 This program reads the json data and calculates monthly highest and monthly lowest stock price,
 monthly average stock price and monthly average stock volume
-'''
+"""
 
 import json
 import pandas as pd
 from collections import defaultdict
 import unittest
+from statistics import mean
 
-with open('tsla_data.json') as f:
-    data = json.load(f)
-    stock = data['meta']['ticker']
-    num_records = data['meta']['found']
-    records = data['data']
+PATH_NAME = "tsla_data.json"
 
-# columns for the json file
-columns = {"date": "object", "open": float, "high": float, "low": float, "close": float, "volume": int}
+with open(PATH_NAME) as f:
+    tsla_df = pd.DataFrame(json.load(f)["data"])
 
-tsla_df = pd.DataFrame(records, columns=columns)
+tsla_df["date"] = pd.to_datetime(tsla_df["date"])
 
-tsla_df['date'] = pd.to_datetime(tsla_df['date'], dayfirst=True)
+# print(tsla_df["date"].dt.month.value_counts().sort_index())
 
-monthly_highest_stock_price, monthly_lowest_stock_price, monthly_average_stock_price, monthly_average_stock_volume = defaultdict(
-    list), defaultdict(list), defaultdict(list), defaultdict(list)
+(
+    monthly_highest_stock_price,
+    monthly_lowest_stock_price,
+    monthly_average_stock_price,
+    monthly_average_stock_volume,
+) = (defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list))
 
 
-def calculate_highest_lowest(date_occurred, stock_pr, mon, price_dict, cal_type):
-    if mon not in price_dict:
-        price_dict[month].extend([date_occurred, stock_pr])
-    if cal_type == 'highest':
-        if price_dict[month][1] < stock_pr:
-            price_dict[month].pop()
-            price_dict[month].pop()
-            price_dict[month].extend([date_occurred, stock_pr])
-    if cal_type == 'lowest':
-        if price_dict[month][1] > stock_pr:
-            price_dict[month].pop()
-            price_dict[month].pop()
-            price_dict[month].extend([date_occurred, stock_pr])
+def calculate_extreme(date_occurred, stock_pr, price_dict, minmax):
+    month = date_occurred.month
+    prev_date, prev_price = price_dict.get(month, (date_occurred, stock_pr))
+    if minmax(prev_price, stock_price) == stock_pr:
+        price_dict[month] = (date_occurred, stock_price)
 
 
 def averages(mon, stock_pr, avg_dict):
@@ -79,37 +75,28 @@ def averages(mon, stock_pr, avg_dict):
 
 
 for index, row in tsla_df.iterrows():
-    date_occurence = row['date']
-    month = row['date'].month
-    stock_price = row['close']
-    stock_vol = row['volume']
-    # calculate the highest stock price for a given month and the corresponding date
-    calculate_highest_lowest(date_occurence, stock_price, month, monthly_highest_stock_price, 'highest')
-    # calculate the lowest stock price for a given month and the corresponding date
-    calculate_highest_lowest(date_occurence, stock_price, month, monthly_lowest_stock_price, 'lowest')
-    # calculate the avg stock price for a given month
+    date_occurence = row["date"]
+    month = row["date"].month
+    stock_price = row["close"]
+    stock_vol = row["volume"]
+    # calculate the highest and lowest stock price for a given month
+    calculate_extreme(date_occurence, stock_price, monthly_highest_stock_price, max)
+    calculate_extreme(date_occurence, stock_price, monthly_lowest_stock_price, min)
+    # calculate the avg stock price and volume for a given month
     averages(month, stock_price, monthly_average_stock_price)
     averages(month, stock_vol, monthly_average_stock_volume)
 
-monthly_avg_price, monthly_avg_volume = defaultdict(int), defaultdict(int)
+
+def calculate_averages(avgs_dict, decimals=2):
+    return {month: round(mean(units), decimals) for month, units in avgs_dict.items()}
 
 
-def calculate_averages(avgs_dict, data_dict):
-
-    for month, units in avgs_dict.items():
-        sum_prices, cnt = 0, 0
-        for i in range(len(units)):
-            sum_prices += units[i]
-            cnt += 1
-        data_dict[month] = round(sum_prices / cnt, 2)
-
-
-calculate_averages(monthly_average_stock_price, monthly_avg_price)
-calculate_averages(monthly_average_stock_volume, monthly_avg_volume)
+# and use it like:
+monthly_avg_price = calculate_averages(monthly_average_stock_price)
+monthly_avg_volume = calculate_averages(monthly_average_stock_volume, decimals=0)
 
 
 class TestMyComputation(unittest.TestCase):
-
     def test_tsla_df(self):
         self.assertGreater(len(tsla_df), 0)
 
@@ -120,9 +107,5 @@ class TestMyComputation(unittest.TestCase):
         self.assertEqual(len(monthly_average_stock_volume), 10)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
-    
-    
-    
