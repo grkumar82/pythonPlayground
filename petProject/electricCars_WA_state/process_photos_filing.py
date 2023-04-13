80883d3b27392a94b00f58a5a4c1f960acc785b0
grkumar82 <72323477+grkumar82@users.noreply.github.com>
2023-04-13 12:28:48 -0700

    Update process_photos_filing.py

--- petProject/electricCars_WA_state/process_photos_filing.py
+++ petProject/electricCars_WA_state/process_photos_filing.py
@@ -16,125 +16,192 @@ from pyspark.sql.window import Window
 
 INPUT_FILE = "electric_vehicles_wa_state.json"
 
-with open(INPUT_FILE) as f:
-    data = json.load(f)
 
-column_names = {}
-column = data["meta"]["view"]["columns"]
+class Process_EV_data:
+    @staticmethod
+    def spark_config():
+        """
+        :return: returns spark config
+        """
+        spark_conf = (
+            SparkConf()
+            .setAppName("Electric_Cars_Analysis")
+            .setMaster("local[*]")
+            .set("spark.driver.memory", "1g")
+            .set("spark.executor.memory", "2g")
+            .set("spark.executor.cores", "2")
+            .set("spark.default.parallelism", "4")
+            .set(
+                "spark.executor.extraJavaOptions",
+                "-XX:+UseG1GC -XX:MaxGCPauseMillis=100",
+            )
+            .set(
+                "spark.driver.extraJavaOptions", "-XX:+UseG1GC -XX:MaxGCPauseMillis=100"
+            )
+        )
+        return spark_conf
 
-(
-    vin_index,
-    model_year_index,
-    make_index,
-    model_index,
-    ev_type_index,
-    electric_range_index,
-) = (0, 0, 0, 0, 0, 0)
+    @staticmethod
+    def spark_schema():
+        """
+        :return: Spark schema
+        """
+        schema = StructType(
+            [
+                StructField("vin", StringType(), True),
+                StructField("model_year", StringType(), True),
+                StructField("make", StringType(), True),
+                StructField("model", StringType(), True),
+                StructField("ev_type", StringType(), True),
+                StructField("electric_range", StringType(), True),
+            ]
+        )
+        return schema
 
-for key, val in enumerate(column):
-    column_name = val["fieldName"]
-    if column_name == "vin_1_10":
-        vin_index = key
-    elif column_name == "model_year":
-        model_year_index = key
-    elif column_name == "make":
-        make_index = key
-    elif column_name == "model":
-        model_index = key
-    elif column_name == "ev_type":
-        ev_type_index = key
-    elif column_name == "electric_range":
-        electric_range_index = key
+    def __init__(self):
+        self.input_file = INPUT_FILE
+        (
+            self.vin_index,
+            self.model_year_index,
+            self.make_index,
+            self.model_index,
+            self.ev_type_index,
+            self.electric_range_index,
+        ) = (0, 0, 0, 0, 0, 0)
+
+        self.column_names = {}
+
+        spark_conf = self.spark_config()
+        # Create a SparkSession with the given configuration
+        self.spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+        self.spark_schema = self.spark_schema()
+        (
+            self.raw_df,
+            self.count_cars_df,
+            self.top_3_cars_df,
+            self.fastest_growing_car_df,
+        ) = (
+            self.spark.createDataFrame([], schema=self.spark_schema),
+            self.spark.createDataFrame([], schema=self.spark_schema),
+            self.spark.createDataFrame([], schema=self.spark_schema),
+            self.spark.createDataFrame([], schema=self.spark_schema),
+        )
+
+    def columns_index(self, input_dict):
+        """
+        :param input_dict: dict
+        :return: tuple
+        takes columns dict and returns columns index
+        """
+        for key, val in enumerate(input_dict):
+            column_name = val["fieldName"]
+            if column_name == "vin_1_10":
+                self.vin_index = key
+            elif column_name == "model_year":
+                self.model_year_index = key
+            elif column_name == "make":
+                self.make_index = key
+            elif column_name == "model":
+                self.model_index = key
+            elif column_name == "ev_type":
+                self.ev_type_index = key
+            elif column_name == "electric_range":
+                self.electric_range_index = key
+
+    def add_values_to_df(self, input_data):
+        """
+        :param input_data: dict
+        :return: Spark dataframe
+        Gets data from the input dataframe
+        """
+        # Create an empty schema for the dataframe
+        df = self.spark.createDataFrame([], schema=self.spark_schema)
+        # hard coding to limit to 1K rows due to limited computed processing of local machine
+        for i in range(len(input_data)):
+            vin = input_data[i][self.vin_index]
+            model_year = str(input_data[i][self.model_year_index])
+            make = str(input_data[i][self.make_index])
+            model = input_data[i][self.model_index]
+            ev_type = input_data[i][self.ev_type_index]
+            electric_range = str(input_data[i][self.electric_range_index])
+            row = (vin, model_year, make, model, ev_type, electric_range)
+            new_row = self.spark.createDataFrame([row], schema=self.spark_schema)
+            df = df.union(new_row)
+        return df
+
+    def process_input_file(self):
+        with open(self.input_file) as f:
+            data = json.load(f)
+        column = data["meta"]["view"]["columns"]
+        self.columns_index(column)
+        self.raw_df = self.add_values_to_df(data["data"])
+
+    def count_cars(self):
+        """
+        :return: None
+        Count of electric cars by model
+        """
+        self.count_cars_df = (
+            self.raw_df.groupBy("model")
+            .agg(count("*").alias("count_cars"))
+            .orderBy(col("count_cars").desc())
+        )
+        self.count_cars_df.show()
+
+    def top_three_cars(self):
+        """
+        :return: None
+        # Top 3 electric cars (by registration) in WA in years 2018 - 202
+        """
+        count_cars = self.raw_df.groupBy("model_year", "model").agg(
+            countDistinct("vin").alias("count_cars")
+        )
+
+        w = Window.partitionBy("model_year").orderBy(col("count_cars").desc())
+
+        self.top_3_cars_df = (
+            count_cars.withColumn("rank_of_car", rank().over(w))
+            .filter(
+                (col("rank_of_car") <= 3) & (col("model_year").cast("bigint") >= 2018)
+            )
+            .orderBy(col("model_year").desc(), col("rank_of_car").asc())
+            .select("model_year", "model", "rank_of_car")
+        )
+        self.top_3_cars_df.show()
+
+    def fastest_growing_car(self):
+        """
+        :return: None
+        # Which electric car saw the fastest increase in registrations in 2022 compared to 2021
+        """
+        count_cars = self.raw_df.groupBy("model_year", "model").agg(
+            countDistinct("vin").alias("cnt_cars")
+        )
+
+        window = Window.partitionBy("model").orderBy("model_year")
+        cars_registration_increase = count_cars.withColumn(
+            "car_count_delta", col("cnt_cars") - lag(col("cnt_cars")).over(window)
+        ).filter(col("model_year").isin("2021", "2022"))
+
+        self.fastest_growing_car_df = (
+            cars_registration_increase.orderBy(col("car_count_delta").desc())
+            .limit(1)
+            .select("model")
+        )
+        # Show the result
+        self.fastest_growing_car_df.show()
+
+    # Stop the SparkSession
+    def stop_spark_session(self):
+        self.spark.stop()
 
 
-# spark config for a local machine
-SPARK_CONF = (
-    SparkConf()
-    .setAppName("Electric_Cars_Analysis")
-    .setMaster("local[*]")
-    .set("spark.driver.memory", "1g")
-    .set("spark.executor.memory", "2g")
-    .set("spark.executor.cores", "2")
-    .set("spark.default.parallelism", "4")
-    .set("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:MaxGCPauseMillis=100")
-    .set("spark.driver.extraJavaOptions", "-XX:+UseG1GC -XX:MaxGCPauseMillis=100")
-)
-
-# Create a SparkSession with the given configuration
-spark = SparkSession.builder.config(conf=SPARK_CONF).getOrCreate()
-
-
-schema = StructType(
-    [
-        StructField("vin", StringType(), True),
-        StructField("model_year", StringType(), True),
-        StructField("make", StringType(), True),
-        StructField("model", StringType(), True),
-        StructField("ev_type", StringType(), True),
-        StructField("electric_range", StringType(), True),
-    ]
-)
-
-# Create an empty schema for the dataframe
-df = spark.createDataFrame([], schema=schema)
-
-# section where the data is within the JSON file.
-raw_json_data = data["data"]
-for i in range(1000):
-    vin = raw_json_data[i][vin_index]
-    model_year = str(raw_json_data[i][model_year_index])
-    make = str(raw_json_data[i][make_index])
-    model = raw_json_data[i][model_index]
-    ev_type = raw_json_data[i][ev_type_index]
-    electric_range = str(raw_json_data[i][electric_range_index])
-    row = (vin, model_year, make, model, ev_type, electric_range)
-    new_row = spark.createDataFrame([row], schema=schema)
-    df = df.union(new_row)
-
-# Count of electric cars by model
-count_cars_df = (
-    df.groupBy("model")
-    .agg(count("*").alias("count_cars"))
-    .orderBy(col("count_cars").desc())
-)
-count_cars_df.show()
-
-# Top 3 electric cars (by registration) in WA in years 2018 - 2022
-count_cars = df.groupBy("model_year", "model").agg(
-    countDistinct("vin").alias("count_cars")
-)
-
-w = Window.partitionBy("model_year").orderBy(col("count_cars").desc())
-
-top_3_cars_df = (
-    count_cars.withColumn("rank_of_car", rank().over(w))
-    .filter((col("rank_of_car") <= 3) & (col("model_year").cast("bigint") >= 2018))
-    .orderBy(col("model_year").desc(), col("rank_of_car").asc())
-    .select("model_year", "model", "rank_of_car")
-)
-
-top_3_cars_df.show()
-
-# Which electric car saw the fastest increase in registrations in 2022 compared to 2021
-count_cars = df.groupBy("model_year", "model").agg(
-    countDistinct("vin").alias("cnt_cars")
-)
-
-window = Window.partitionBy("model").orderBy("model_year")
-cars_registration_increase = count_cars.withColumn(
-    "car_count_delta", col("cnt_cars") - lag(col("cnt_cars")).over(window)
-).filter(col("model_year").isin("2021", "2022"))
-
-fastest_growing_car_df = (
-    cars_registration_increase.orderBy(col("car_count_delta").desc())
-    .limit(1)
-    .select("model")
-)
-# Show the result
-fastest_growing_car_df.show()
-
-# Stop the SparkSession
-spark.stop()
+test_run = Process_EV_data()
+test_run.process_input_file()
+test_run.count_cars()
+test_run.top_three_cars()
+test_run.fastest_growing_car()
+test_run.stop_spark_session()
 
 # Outputs when limited to only 100 rows due to limited processing power on my laptop
 # count_cars_df
